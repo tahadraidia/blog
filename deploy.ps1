af5f78bca97c425d68522bce7f7ef518e0f0fd47
Taha <taha.ibrahim.draidia@gmail.com>
2022-01-11 06:18:20 +0000

    Using WSL instead of Windows native tools.

--- deploy.ps1
+++ deploy.ps1
@@ -4,4 +4,7 @@ $SSH_USER="blogger"
 $SSH_TARGET_DIR="/home/blogger/www/"
 $OUTPUTDIR=".\public\"
 
-scp -P $SSH_PORT -r "$OUTPUTDIR"* "$SSH_USER@${SSH_HOST}:$SSH_TARGET_DIR"
+#scp -P $SSH_PORT -r "$OUTPUTDIR"* "$SSH_USER@${SSH_HOST}:$SSH_TARGET_DIR"
+# Somehow, this PS1 script does not work as intended, hence we use SHELL script with WSL instead.
+# Due the usage of different laptops, WSL won't be installed on all of them so we keep old PS1 code here.
+wsl --cd $PWD ./deploy.sh
